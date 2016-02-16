0c2196a6365b8467119cf283e7909de34ee4a118
GabbyGeier123 <ggeier@lungcanceralliance.org>
2016-02-16 10:04:45 -0500

    javascript

---
+++ js/index_About.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#ex").click(function(){
+        $("#1").slideToggle();
+    });
+
+    $("#sk").click(function(){
+        $("#2").slideToggle();
+    });
+
+    $("#ed").click(function(){
+    $("#3").slideToggle();
+    });
+    });
