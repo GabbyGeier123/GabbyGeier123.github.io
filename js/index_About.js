4bb2c61c60fa9d1e8c368b2afb741eeb1eea729f
GabbyGeier123 <ggeier@lungcanceralliance.org>
2016-02-21 18:48:51 -0500

    adjustments to project

--- js/index_About.js
+++ js/index_About.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
 
     $("#whitebox").hide();
+    $("#ReadLess").hide();
+    $("#total").hide();
 
-    $("#ReadMore").click(function(){
-        $("#whitebox").slideDown();
+     $("#ReadMore").click(function(){
+ 		 $("#total").slideDown();
+ 		 $("#whitebox").slideDown();
+        $("#ReadLess").show();
         $("#ReadMore").hide();
+    });
+
+        $("#ReadLess").click(function(){
+        $("#total").slideUp();
+        $("#whitebox").slideUp();
+        $("#ReadMore").show();
+        $("#ReadLess").hide();
+    });
+
+    $("#menu").click(function(){
+     $("#header").toggleClass("expand");
+     $("#menu").hide();
+    });
+
+    $("#header").click(function(){
+     $("#header").toggleClass("expand");
+     $("#header").hide();
+     $("#menu").show();
     });
         });
